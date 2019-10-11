7329aadabaaf83404d110dd82effcbff1ec259e6
Irina Stepanova <stira_04@mail.ru>
2019-10-11 18:12:43 +0600

    feat: add guessing-game-task solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,26 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        // задаем диапазон поиска
+        this.minValue = min;
+        this.maxValue = max;
     }
 
     guess() {
-
+        // предлагаем в качестве искомого значение из середины диапазона
+        this.resultGuess = Math.round((this.minValue + this.maxValue)/2);
+        // проверяем результат
+        return this.resultGuess;
     }
 
     lower() {
-
+        // если искомое значение меньше, убираем верхнюю половину диапазона
+        this.maxValue = this.resultGuess;
     }
 
     greater() {
-
+        // если искомое значение больше, убираем нижнюю половину диапазона
+        this.minValue = this.resultGuess;
     }
 }
 
